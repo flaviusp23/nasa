94693c62546e8c365dd324d27c4fb5cb328e505d
flaviusp23 <flavius.paltin@yahoo.com>
2024-08-07 18:19:36 +0300

    mongoose models

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const mongoose = require('mongoose')
 const app = require('./app');
 
 const {loadPlanetsData} = require('./models/planets.model')
@@ -7,7 +8,18 @@ const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
+const MONGO_URL = 'mongodb+srv://flaviuspaltin599:1234@nasacluster.y81ag.mongodb.net/nasa?retryWrites=true&w=majority&appName=NASACluster';
+
+mongoose.connection.once('open',() => {
+    console.log('MongoDB connection ready!');
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log(err);
+});
+
 async function startServer(){
+    await mongoose.connect(MONGO_URL);
     await loadPlanetsData();
 
     server.listen(PORT, ()=>{
