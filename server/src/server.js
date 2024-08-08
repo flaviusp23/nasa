f5eacf147855e4b1f87356f09b4e8793557e0529
flaviusp23 <flavius.paltin@yahoo.com>
2024-08-08 16:26:06 +0300

    getting all launches from spacex api

--- server/src/server.js
+++ server/src/server.js
@@ -4,6 +4,8 @@ const app = require('./app');
 
 const { loadPlanetsData } = require('./models/planets.model');
 const { mongoConnect } = require('./services/mongo');
+const { loadLaunchesData } = require('./models/launches.model');
+
 
 const PORT = process.env.PORT || 8000;
 
@@ -12,6 +14,7 @@ const server = http.createServer(app);
 async function startServer(){
     await mongoConnect();
     await loadPlanetsData();
+    await loadLaunchesData();
 
     server.listen(PORT, ()=>{
         console.log(`Listening on port ${PORT}...`) 
